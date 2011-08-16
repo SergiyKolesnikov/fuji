571a7afd7b39fe9a285af393a8a003cd765b4658
Claus Hunsen <hunsen@fim.uni-passau.de>
2011-08-16 11:40:18 +0000

    Simple test case for fix added.  Tags directory added.

---
+++ tests/FIXSimple/F1/A.java
@@ -0,0 +1,8 @@
+//F1
+public class A {
+    public static void m1(String s) {
+        System.out.println("F1::::A.m1()");
+    }
+
+    public static void main(String[] args) { m1(); }
+}
---
+++ tests/FIXSimple/F2/B.java
@@ -0,0 +1,9 @@
+//F2
+public class B {
+    public static void m2() {
+        A.m1("");
+        System.out.println("F2::::B.m2()");
+    }
+
+    public static void main(String[] args) { m2(); }
+}
