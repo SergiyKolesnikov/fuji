f77c98fddd7b0f0fe336d6cd427e94572126eb86
Peter Lutz <p_lutz@gmx.de>
2012-07-09 15:30:10 +0000

    kommentiert

--- FujiCompiler/src/fuji/TypeErrorInfo.java
+++ FujiCompiler/src/fuji/TypeErrorInfo.java
@@ -2,10 +2,15 @@ package fuji;
 
 import java.util.HashSet;
 
+/** Keeps info about:<ul><li>the IDs of the target features, 
+ * that implement a certain type, and</li><li>source file names and line numbers 
+ * of accesses of a certain type</li></ul>*/
 public class TypeErrorInfo {
 	
+	/* IDs of the target features, that implement a certain type */
 	private HashSet<Integer> toFeatureIDs = new HashSet<Integer>();
 	
+	/* source file names and line numbers of accesses of a certain type */
 	private HashSet<String> accessSourceInfo = new HashSet<String>();
 	
 	public TypeErrorInfo(HashSet<Integer> toFeatureIDs, 
