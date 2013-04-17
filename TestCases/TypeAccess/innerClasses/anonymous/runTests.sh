abdd7475375c3c2563448b630235b61eb0191402
Peter Lutz <p_lutz@gmx.de>
2013-04-17 12:11:36 +0000

    Optionen für "find" umgestellt und POSIX-kompatibel

--- TestCases/TypeAccess/innerClasses/anonymous/runTests.sh
+++ TestCases/TypeAccess/innerClasses/anonymous/runTests.sh
@@ -20,7 +20,7 @@ echoOK() {
 }
 
 # For all numbered folders in this folder:
-for Dir in $(basename $(find . -type d -d 1 -name "[0-9][0-9]*")); do
+for Dir in $(basename $(find . -depth 1 -type d -name "[0-9][0-9]*")); do
 
   # 01. Model with errors
   # ---------------------
