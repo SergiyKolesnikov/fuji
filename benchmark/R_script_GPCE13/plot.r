d2181de0bf5fab55d21e0d9ef07676e166470490
Claus Hunsen <hunsen@fim.uni-passau.de>
2013-05-10 14:24:54 +0000

    updated R script with family data. fixed log-scale.

--- benchmark/R_script_GPCE13/plot.r
+++ benchmark/R_script_GPCE13/plot.r
@@ -1,7 +1,11 @@
+draft = F
+if (draft) dev.off()
+
+
 options(width=180)
 
 ca=commandArgs(trailingOnly=TRUE) #only args after --args
-#ca=c("EPL", "GPL", "GUIDSL", "Prevayler")
+if (draft) ca=c("EPL", "GPL", "GUIDSL", "Prevayler")
 
 #interpret all elements of ca as names of case studies
 caseStudies = ca[!is.na(ca)]
@@ -9,6 +13,7 @@ csInternalData = vector("list",length(caseStudies))
 csExternalData = vector("list",length(caseStudies))
 csExternalData_features = vector("list",length(caseStudies))
 csInternalData_features = vector("list",length(caseStudies))
+csInternalData_fam = vector("list",length(caseStudies))
 for (i in 1:length(caseStudies)) {
   cat("reading case study data \"",caseStudies[i],"\"\n", sep="")
   int = read.csv(file=paste("./",caseStudies[i],"/inttimetypechecker.csv",sep=""),head=TRUE, sep="\t", na.strings=c("","NA"))
@@ -19,6 +24,10 @@ for (i in 1:length(caseStudies)) {
   
   csInternalData_features[[i]][is.na(csInternalData_features[[i]])] <- c(0)
   int[is.na(int)] <- c(0)
+  
+  csInternalData_fam[[i]] = int[int$variant == "family", ]
+  int = int[int$variant != "family", ]
+  ext = ext[ext$variant != "family", ]
   
   if (nrow(int) != nrow(ext)) {
     stop("inconsistent input tables")
@@ -32,26 +41,34 @@ for (i in 1:length(caseStudies)) {
 
 getFeatureTimeInt <- function(caseStudyID) {
   prodlines = csInternalData_features[[caseStudyID]]
-  prodlines = prodlines[prodlines$variant != "family", ]
   c(sum((prodlines$ASTcomp)),sum((prodlines$typecheck)))
 }
 getProdTimeInt <- function(caseStudyID) {
   prodlines = csInternalData[[caseStudyID]]
-  prodlines = prodlines[prodlines$variant != "family", ]
+  c(sum((prodlines$ASTcomp)),sum((prodlines$typecheck)))
+}
+getFamTimeInt <- function(caseStudyID) {
+  prodlines = csInternalData_fam[[caseStudyID]]
   c(sum((prodlines$ASTcomp)),sum((prodlines$typecheck)))
 }
 
-plotData <- matrix(nrow=4, ncol=length(caseStudies))
+plotData <- matrix(nrow=2, ncol=length(caseStudies))
 for (i in 1:length(caseStudies)) {
-  plotData[,i] = rbind(as.matrix(getProdTimeInt(i)), as.matrix(c(0,0))) # feature time
+  plotData[,i] = getProdTimeInt(i) # product time
 }
 print (plotData)
 
-plotDataFeat <- matrix(nrow=4, ncol=length(caseStudies))
+plotDataFeat <- matrix(nrow=2, ncol=length(caseStudies))
 for (i in 1:length(caseStudies)) {
-  plotDataFeat[,i] = rbind(as.matrix(c(0,0)), as.matrix(getFeatureTimeInt(i))) # feature time
+  plotDataFeat[,i] = getFeatureTimeInt(i) # feature time
 }
 print (plotDataFeat)
+
+plotDataFam <- matrix(nrow=2, ncol=length(caseStudies))
+for (i in 1:length(caseStudies)) {
+  plotDataFam[,i] = getFamTimeInt(i) # fam time
+}
+print (plotDataFam)
 
 ex_color <- rgb(51, 102, 255, maxColorValue=255)
 bdd_color <- rgb(204, 0, 51, maxColorValue=255)  
@@ -60,43 +77,69 @@ intEq_color <- rgb(153,115,0, maxColorValue=255)
 intAdd_color <- rgb(0,204,51, maxColorValue=255)
 pred_color <- rgb(153, 0, 204, maxColorValue=255)
 
-getMaxY <- function(plotData, plotDataFeat) {
+getMaxY <- function(plotData, plotDataFeat, plotDataFam) {
   maximum = 0
   for (i in 1:ncol(plotData)) {
-    maximum = max( sum(plotData[,i]), sum(plotDataFeat[,i]), maximum)
+    maximum = max( sum(plotData[,i]), sum(plotDataFeat[,i]), sum(plotDataFam[,i]), maximum)
   }
   maximum
 }
 
-yLimits=c(1,getMaxY(plotData, plotDataFeat)) # logarithmic plot, so we need to make the y axis larger
+yLimits=c(1,getMaxY(plotData, plotDataFeat, plotDataFam)) # logarithmic plot, so we need to make the y axis larger
 xLimits=c(0,length(caseStudies)*4)
 color <- c("lightblue", "lightskyblue", # prod
-            "tomato2", "red2", "firebrick3", # feat
-            "olivedrab2", "lawngreen", "chartreuse3" # fam
+            "tomato2", "firebrick3", # feat
+            "olivedrab2", "chartreuse3" # fam
 )
 
-
-pdf(file=paste("plot_int.","pdf",sep=""), width=8, height=5, onefile=TRUE, paper="special") 
+if (!draft) pdf(file=paste("plot_int.","pdf",sep=""), width=8, height=5, onefile=TRUE, paper="special") 
 
 for (i in 1:length(caseStudies)) {  
   par(new=TRUE)
-  barplot(cbind( t(t(plotData[,i])), t(t(plotDataFeat[,i])) ), #, t(t(as.matrix(c(10000,10000,10000,10000))))
+  barplot(t(t(plotData[,i])), #, t(t(as.matrix(c(10000,10000,10000,10000))))
         #beside=TRUE,
-        space=c((i-1)*3, 0.1),
-        col=color,
+        space=c((i-1)*4.0, 0.1),
+        col=color[1:2],
         ylim=yLimits,
         xlim=xLimits,
-        xaxt="n"
-        #log="y",
+        xaxt="n",
+        log="y",
   )
+  par(new=TRUE)
+  barplot(t(t(plotDataFeat[,i])), #, t(t(as.matrix(c(10000,10000,10000,10000))))
+          #beside=TRUE,
+          space=c((i-1)*4.0+1.1, 0.1),
+          col=color[3:4],
+          ylim=yLimits,
+          xlim=xLimits,
+          xaxt="n",
+          yaxt="n",
+          log="y",
+  )
+  par(new=TRUE)
+  barplot(t(t(plotDataFam[,i])), #, t(t(as.matrix(c(10000,10000,10000,10000))))
+          #beside=TRUE,
+          space=c((i-1)*4.0+2.2, 0.1),
+          col=color[5:6],
+          ylim=yLimits,
+          xlim=xLimits,
+          xaxt="n",
+          yaxt="n",
+          log="y",
+  )
+#t(t(plotDataFeat[,i])) 
 }
 
-legend("topright",c('product-based ASTComp','product-based typecheck','feature-based ASTComp','feature-based typecheck'), inset = .01, fill=color)
-positions=(((0:(length(caseStudies)-1)) * 3.0 ) ) # begin of case study
-axis(1, pos=1.1, at=positions+1.1, labels=caseStudies, cex.axis=1, tick=FALSE, las=3) #labels
+legend("top",
+       c('product-based ASTComp','product-based typecheck',
+         'feature-based ASTComp','feature-based typecheck',
+         'family-based ASTComp','family-based typecheck'),
+       inset = .01, fill=color, cex=0.6)
+positions=(((0:(length(caseStudies)-1)) * 4.0 ) ) # begin of case study
+axis(1, pos=1.1, at=positions+1.6, labels=caseStudies, cex.axis=1, tick=FALSE, las=3) #labels
 axis(1, at=positions+0.5, labels=rep("",length(caseStudies)), cex.axis=1)
 axis(1, at=positions+1.6, labels=rep("",length(caseStudies)), cex.axis=1)
-#axis(1, at=positions+2.625, labels=rep("",length(caseStudies)), cex.axis=1)
+axis(1, at=positions+2.7, labels=rep("",length(caseStudies)), cex.axis=1)
 
 warnings()
-dev.off()
+if (!draft) dev.off()
