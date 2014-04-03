43f2056cc25bd11b6273d6dab74853191ca5beb7
Sergiy Kolesnikov <kolesnik@fim.uni-passau.de>
2014-04-03 17:18:23 +0200

    Add test case for the field writes closure analysis.
    In this test case the wirtten field is an array and the writes are done
    to the field and to the elements of the array.

---
+++ tests/FieldWritesClosureArray/Base/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+
+    public String[] name;
+
+    public Node() {
+        this.name = new String[1]; // write
+        init();
+        String n = name[0];
+        Node name = new Node();
+        name.name[0] = n; // write
+        n = name.name[0];
+    }
+
+    public void init() {
+        name[0] = "init"; // write
+    }
+}
